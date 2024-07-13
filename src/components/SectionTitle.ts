22a12b7024315458c874062eab503c3cde344a2f
Kozzzlova <kozlova.jully@gmail.com>
2024-07-14 00:53:36 +0300

    fixed sectionTitle

--- src/components/SectionTitle.ts
+++ src/components/SectionTitle.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { Theme } from '../styles/Theme';
 import { font } from '../styles/Common';
 
-export const SectionTitle: React.FC = styled.h2`
+export const SectionTitle = styled.h2`
    font-size: clamp(
       60px,
       calc((100vw - 375px) / (1440 - 375) * (150 - 60) + 60px),
